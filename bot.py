e2c0be8890e426ba59c5b49c2f79e43c5c3baeec
Colin <colin.mcallister89@yahoo.com>
2023-04-20 14:53:51 -0400

    removing separate method for status

--- bot.py
+++ bot.py
@@ -8,19 +8,12 @@ bot = commands.Bot()
 
 @bot.event
 async def on_ready():
+    bot.change_presence(activity=discord.Game("seeking contributors!"))
     print(f"{bot.user} is ready and online!")
     
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
-
-# @tasks.loop(minutes=1.0)
-async def status_task() -> None:
-    """
-    Setup the game status task of the bot
-    """
-    statuses = ["seeking contributors!"]
-    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
             
 with open('config.json') as f:
     config = json.load(f)
